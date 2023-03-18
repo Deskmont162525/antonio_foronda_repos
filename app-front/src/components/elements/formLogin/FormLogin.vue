b28d69afbb31246430cedb7eaf5793f3bdbdb89f
antonio foronda <deskmont16@outlook.es>
2023-03-17 23:43:10 -0500

    inicio de la pagina login con scss

---
+++ app-front/src/components/elements/formLogin/FormLogin.vue
@@ -0,0 +1,29 @@
+<script>
+import InputType from '../../ui/InputType/InputType.vue';
+export default {
+    data() {
+        return {
+            inputName: 'email',
+            inputLabel: 'Email Address',
+            inputType: 'email',
+        };
+    },
+    components: { InputType }
+}
+</script>
+
+<template>
+    <div class="Container-principal__form">
+        <div class="Container-principal__form__content">
+            <h2>Enter your credentials</h2>
+            <InputType :name="inputName" :label="inputLabel" :type="inputType" />
+        </div>
+    </div>
+</template>
+  
+
+  
+<style lang="scss">
+/* Estilos específicos del componente */
+@import './formLoginStyle.scss';
+</style>
